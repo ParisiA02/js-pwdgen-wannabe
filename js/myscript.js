ba6a26ae0e57dd1f8207839aeabebc27fa8bcd5d
Andrea Parisi <andreaparisi4502@gmail.com>
2021-10-20 16:27:00 +0200

    fix exercise

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,13 @@
 //dichiarazione delle variabili
-let nome, cognome, colore;
-
+let nome, cognome, colore, finalPassword;
+let addToPassword = "21";
 //input dei dati
 nome = prompt("Inserisci il tuo nome: ");
 cognome = prompt("Inserisci il tuo cognome: ");
 colore = prompt("Inserisci il tuo colore preferito: ");
 
+//elaborazione 
+finalPassword = nome + cognome + colore + addToPassword;
+
 //output dei dati
-document.getElementById("mio_id").innerHTML = "La password assegnata è: " + nome + cognome + colore + "21";
+document.getElementById("mio_id").innerHTML = "La password assegnata è: " + finalPassword;
